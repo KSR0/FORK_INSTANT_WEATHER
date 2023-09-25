9c0991b1a4dc8ed60f7b7d958dc1bea3a84f2b13
Nicolas <nicolas.peyregne@etu.unicaen.fr>
2023-09-25 12:43:28 +0200

    fetch api

--- js/api_commune.js
+++ js/api_commune.js
@@ -1,14 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
+    
+    ///////
+    function createDropDownList() {
+        
+    }
+    ///////
+    ///////
+    function warnUserPostalCodeNotValid() {
+        console.error("Postal code not valid");
+    }
 
-    const url_commune = "https://geo.api.gouv.fr/communes?codePostal=78000";
-    console.log("true");
+    function onlyAllowPostalCode(postal_code) {
+        if (postal_code <= 0 || postal_code > 95999) {
+            warnUserPostalCodeNotValid();
+        }
+    }
+    ///////
+    
     
     document.querySelector("#postal_code").addEventListener("input", function () {
-        console.log(this.value);
+        onlyAllowPostalCode(this.value);
+
+        let url_commune = `https://geo.api.gouv.fr/communes?codePostal=${this.value}`;
+        //console.log(url_commune);
+        fetch(url_commune)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network reponse was not ok");
+            }
+            return response.json();
+        })
+        .then(data => {
+            createDropDownList();
+            console.log(data[0].nom);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+        
+
     })
-
-    
-
-
 
 });
