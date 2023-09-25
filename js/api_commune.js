a1c6a030de296074fdd98fa5c9da4e5c94dd2765
KSR0 <nicolas.peyregne@gmail.com>
2023-09-25 19:51:14 +0200

    Possible to get insee code from dropDownList

--- js/api_commune.js
+++ js/api_commune.js
@@ -1,53 +1,78 @@
 document.addEventListener("DOMContentLoaded", function () {
     
     ///////
-    function createDropDownList() {
+    function eraseDropDownList() {
+        document.querySelector(".drop_down_list").innerHTML = ``;
+    }
 
+    function fillDropDownList(donnee_ville) {
+        document.querySelector("#city-select").innerHTML += 
+            `
+            <option value="${donnee_ville.code}">${donnee_ville.nom}</option>
+            `;
+    }
+
+    function createDropDownList() {
+        document.querySelector(".drop_down_list").innerHTML = 
+            `<label for="city_select">Choisir une ville :</label>
+            <select name="cities" id="city-select">
+            </select>`;
     }
     ///////
+
+
+
     ///////
     function warnUserPostalCodeNotValid() {
+        // Cree un signal visuel pour signaler à l'utilisateur un probleme
         console.error("Postal code not valid");
     }
 
-    function onlyAllowPostalCode(postal_code) {
-        if (postal_code <= 0 || postal_code > 95999) {
-            warnUserPostalCodeNotValid();
-        }
+    function warnUserPostalCodeNotExisting() {
+        console.error("Postal code not existing");
     }
     ///////
-    
+
+
     
     document.querySelector("#postal_code").addEventListener("input", function () {
-        onlyAllowPostalCode(this.value);
+        if (this.value.length == 5) {
+            let url_commune = `https://geo.api.gouv.fr/communes?codePostal=${this.value}`;
+            //console.log(url_commune);
+            fetch(url_commune)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network reponse was not ok");
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data != "") {
+                    //console.log(data);
+                    createDropDownList();
+                    for (let ville of data) {
+                        fillDropDownList(ville);
+                    }
+                } else {
+                    warnUserPostalCodeNotExisting();
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        } else {
+            eraseDropDownList();
+            warnUserPostalCodeNotValid();
+        }
 
-        let url_commune = `https://geo.api.gouv.fr/communes?codePostal=${this.value}`;
-        //console.log(url_commune);
-        fetch(url_commune)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network reponse was not ok");
-            }
-            return response.json();
-        })
-        .then(data => {
-            createDropDownList();
-            console.log(data[0].nom);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+        
         
 
 
     })
 
-    /*
-    <div class="drop_down_list">
-            <label for="city_select">Choisir une ville :</label>
-            <select name="cities" id="city-select">
-                <option value="14123">Ifs</option>
-            </select>
-        </div>
-    */
+    document.querySelector("#researching").addEventListener("click", function() {
+        console.log(document.querySelector("#city-select").value);
+    });
+
 });
