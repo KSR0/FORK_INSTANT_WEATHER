d7e4350618d2a239602864551916a41530a92cb0
Bayon Axel <axel.bayon14@gmail.com>
2023-10-03 09:31:54 +0200

    Big update for CSS + adding an "last actualisation" message

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -1,43 +1,56 @@
-document.addEventListener("DOMContentLoaded",() => {
-
-    const weatherTab = ["Soleil","Peu nuageux","Ciel voilé","Nuageux"]
+document.addEventListener("DOMContentLoaded", () => {
+    const weatherTab = ["Soleil","Peu nuageux","Ciel voilé","Nuageux"];
+    const daysOfWeek = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
     document.querySelector("#searchBtn").addEventListener("click", function() {
         cityInsee = document.querySelector("#city_select").value;
-        let div = document.getElementById("dropDownCard")
+        let div = document.getElementById("dropDownCard");
 
         fetch(`https://api.meteo-concept.com/api/forecast/daily?token=52ebc4fdc1af7f1041b873d5f7c3140eaa23454977b3519f5f1110a3a8176c48&insee=${cityInsee}`)
-
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.json();
         })
-
         .then(data => {
-            if(!data.city.insee.length > 5 || !data.city.insee.length <5) 
-            console.log(data);
-            {
-                div.innerHTML = `
+            if (data.city.insee.length !== 5) {
+                console.log(data);
+                return;
+            }
+
+            const now = new Date();
+            const hours = now.getHours();
+            const minutes = now.getMinutes();
+            const seconds = now.getSeconds(); // Ajout des secondes
+            const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`; // Ajout des secondes
+
+            const dayOfWeek = daysOfWeek[now.getDay()];
+            const dayOfMonth = now.getDate();
+            const month = months[now.getMonth()];
+            const year = now.getFullYear();
+
+            const formattedDate = `${dayOfWeek} ${dayOfMonth.toString().padStart(2, '0')} ${month} ${year}`;
+
+            div.innerHTML = `
+                <div id="actualisation">
+                    <p id="actualisation">Dernière actualisation à :<span class="bold underline">${formattedTime}</span><br>pour :<span class="bold underline">${data.city.name}</span></p>
+                </div>
+
                 <div id="dropDownCardChild">
-                    <h2>${data.city.name}</h2>
+                    <h2 id="day">${formattedDate}</h2>
+                    <p id="temperature">Temp° minimale : <span id="temperature_min">${data.forecast[0].tmin}°C</span></p>
+                    <p id="temperature">Temp° maximale : <span id="temperature_max"> ${data.forecast[0].tmax}°C</span></p>
                     <h3>${weatherTab[data.forecast[0].weather]}
                         <span>Ensoleillement ${data.forecast[0].sun_hours}H</span>
                         <span>Pluie ${data.forecast[0].probarain}%</span>
                     </h3>
-                    <h2>Minimale</h2>
-                    <h2>${data.forecast[0].tmin}°C</h2>
-                    <h2>Maximale</h2>
-                    <h2>${data.forecast[0].tmax}°C</h2>
                 </div>
-                `;
-            }
+            `;
         })
-
         .catch(error => {
-            console.error('There was a problem with the fetch operation',error);
-        })
+            console.error('There was a problem with the fetch operation', error);
+        });
     });
-
-})
+});
