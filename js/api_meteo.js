facfd953cdc29babcecfa45c0d8214f4b3dfdcdc
Bayon Axel <axel.bayon14@gmail.com>
2023-10-03 12:25:01 +0200

    Adjusting emojis marges and values of temperatures

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -40,12 +40,12 @@ document.addEventListener("DOMContentLoaded", () => {
                     <p id="actual_weather">${weatherTab[data.forecast[0].weather]}</p>
 
                     <p id="temperature">
-                        <i class="fa-solid fa-temperature-high fa-xl" style="color: #24e5ff;"></i>
+                        <i class="fa-solid fa-temperature-low fa-xl" style="color: #24e5ff;"></i>
                         <span id="temperature_min">${data.forecast[0].tmin}°C</span>
                     </p>
 
                     <p id="temperature">
-                        <i class="fa-solid fa-temperature-low fa-xl" style="color: #ff5024;"></i>
+                        <i class="fa-solid fa-temperature-high fa-xl" style="color: #ff5024;"></i>
                         <span id="temperature_max"> ${data.forecast[0].tmax}°C</span>
                     <p>
 
@@ -54,14 +54,14 @@ document.addEventListener("DOMContentLoaded", () => {
                         <div id="sun_time">
                             <p>
                                 <i class="fa-solid fa-sun fa-spin fa-xl" style="color: #ffe75f;"></i>
-                                 Durée d'ensoleillement :<span id="sun_time_value" class="bold underline">${data.forecast[0].sun_hours}H</span>
+                                 Durée d'ensoleillement :<span id="sun_time_value" class="bold">${data.forecast[0].sun_hours}H</span>
                             </p>
                         </div>
 
                         <div id="raining_pourcentage">
                             <p>
                                 <i class="fa-solid fa-droplet fa-beat-fade fa-xl" style="color: #c7d8fa;"></i>
-                                   Pluie :<span id="raining_pourcentage_value" class="bold underline">${data.forecast[0].probarain}%</span>
+                                   Pluie :<span id="raining_pourcentage_value" class="bold">${data.forecast[0].probarain}%</span>
                             </p>
                         </div>
                     
